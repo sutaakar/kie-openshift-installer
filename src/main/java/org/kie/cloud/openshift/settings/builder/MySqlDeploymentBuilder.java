55b01979b49658f4ec08c0577dc0e894cb7b9d40
Karel Suta <ksuta@redhat.com>
2018-07-05 14:31:50 +0200

    Move persistence configuration to AbstractDeploymentBuilder

--- src/main/java/org/kie/cloud/openshift/settings/builder/MySqlDeploymentBuilder.java
+++ src/main/java/org/kie/cloud/openshift/settings/builder/MySqlDeploymentBuilder.java
@@ -15,20 +15,6 @@
  */
 package org.kie.cloud.openshift.settings.builder;
 
-import java.util.Collections;
-import java.util.List;
-
-import io.fabric8.kubernetes.api.model.Container;
-import io.fabric8.kubernetes.api.model.HasMetadata;
-import io.fabric8.kubernetes.api.model.PersistentVolumeClaim;
-import io.fabric8.kubernetes.api.model.PersistentVolumeClaimBuilder;
-import io.fabric8.kubernetes.api.model.PodSpec;
-import io.fabric8.kubernetes.api.model.Quantity;
-import io.fabric8.kubernetes.api.model.Volume;
-import io.fabric8.kubernetes.api.model.VolumeBuilder;
-import io.fabric8.kubernetes.api.model.VolumeMount;
-import io.fabric8.kubernetes.api.model.VolumeMountBuilder;
-import io.fabric8.openshift.api.model.DeploymentConfig;
 import io.fabric8.openshift.api.model.Template;
 import org.kie.cloud.openshift.OpenShiftImageConstants;
 
@@ -60,42 +46,8 @@ public class MySqlDeploymentBuilder extends AbstractDeploymentBuilder {
     }
 
     public MySqlDeploymentBuilder makePersistent(String persistentVolumeClaimSize) {
-        for (DeploymentConfig mySqlDeploymentConfig : getDeployment().getDeploymentConfigs()) {
-            PodSpec mySqlSpec = mySqlDeploymentConfig.getSpec().getTemplate().getSpec();
-            List<Container> mySqlContainers = mySqlSpec.getContainers();
-
-            if (mySqlContainers.size() > 1) {
-                throw new RuntimeException("Corrent configuration doesn't support multiple containers in Deployment config.");
-            }
-
-            // TODO application name just has to be set somehow
-            VolumeMount mySqlVolumeMount = new VolumeMountBuilder().withName("${APPLICATION_NAME}-mysql-pvol")
-                                                                   .withMountPath("/var/lib/mysql/data")
-                                                                   .build();
-            mySqlContainers.get(0).getVolumeMounts().add(mySqlVolumeMount);
-
-            Volume mySqlVolume = new VolumeBuilder().withName("${APPLICATION_NAME}-mysql-pvol")
-                                                    .withNewPersistentVolumeClaim()
-                                                        .withClaimName("${APPLICATION_NAME}-mysql-claim")
-                                                    .endPersistentVolumeClaim()
-                                                    .build();
-            mySqlSpec.getVolumes().add(mySqlVolume);
-
-            PersistentVolumeClaim mySqlPersistentVolumeClaim = new PersistentVolumeClaimBuilder().withKind("PersistentVolumeClaim")
-                                                                                                 .withNewMetadata()
-                                                                                                     .withName("${APPLICATION_NAME}-mysql-claim")
-                                                                                                 .endMetadata()
-                                                                                                 .withNewSpec()
-                                                                                                     .withAccessModes("ReadWriteOnce")
-                                                                                                     .withNewResources()
-                                                                                                         .withRequests(Collections.singletonMap("storage", new Quantity(persistentVolumeClaimSize)))
-                                                                                                     .endResources()
-                                                                                                 .endSpec()
-                                                                                                 .build();
-            List<HasMetadata> objects = getDeployment().geTemplate().getObjects();
-            objects.add(mySqlPersistentVolumeClaim);
-            getDeployment().geTemplate().setObjects(objects);
-        }
+        // TODO make PV size hardcoded?
+        addPersistence("${APPLICATION_NAME}-mysql", "/var/lib/mysql/data", "ReadWriteOnce", persistentVolumeClaimSize);
         return this;
     }
 }
